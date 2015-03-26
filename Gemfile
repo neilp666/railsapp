508adbc5c9632bfddb2e3fe35d7033a1eb1dc153
neil patel <neil@myindianweddingapp.com>
2015-03-26 15:31:49 +0000

    Added controller

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'bootstrap-sass'
+gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
 
-
+#figaro 
+gem 'figaro'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
+
+  #support stripe payments through charges
+  resources :charges
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
