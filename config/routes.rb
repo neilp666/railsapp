642628c6fcf6d6a27e3648185e34fd13b66e548f
neil patel <neil@myindianweddingapp.com>
2015-03-27 11:11:34 +0000

    Added the ability to store purchases

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   #support stripe payments through charges
   resources :charges
+
+  resources :purchases, only: [:show]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
